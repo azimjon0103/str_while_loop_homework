79fa88396e4232ace665c2bca1b682310ae231d8
azimjon0103 <azimjonfayzullayev3@gmail.com>
2022-10-20 14:25:51 +0500

    6

--- While06.py
+++ While06.py
@@ -11,10 +11,11 @@ def main(s):
     ans=0
     s=s.lower()
     while  i<len(s):
-        if s[i]=='a' or s[i]=='e' or s[i]=='i' or s[i]=='o' or s[i]=='u':
-            ans+=0
-        else :
-            ans+= 1   
+        if s[i].isalpha():
+            if s[i]=='a' or s[i]=='e' or s[i]=='i' or s[i]=='o' or s[i]=='u':
+                ans+=0
+            else : 
+                ans+= 1   
         i+=1          
     return ans
 print(main('CodecshoolUz'))    
