0e0aff514e20609f14a913f646ef78d9b4059a69
azimjon0103 <azimjonfayzullayev3@gmail.com>
2022-10-20 15:56:19 +0500

    10

--- While10.py
+++ While10.py
@@ -6,4 +6,13 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i=0
+    ans=0
+    while  i<len(s):
+        if s[i].isdigit():
+            if int(s[i])%2==1:
+                   ans+=1    
+        i+=1          
+    sum=ans+i    
+    return sum
+print(main('113540'))
