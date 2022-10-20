a743e05dc70c19fa290e12b66f3a7065b7e1d829
azimjon0103 <azimjonfayzullayev3@gmail.com>
2022-10-20 15:16:42 +0500

    8

--- While08.py
+++ While08.py
@@ -6,4 +6,12 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i=0
+    ans=0
+    while  i<len(s):
+        if s[i].isdigit():
+            if int(s[i])%2==1:
+                   ans+=1    
+        i+=1          
+    return ans
+print(main('1026503'))   
