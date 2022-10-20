e40b412c71d4ea08450630acb9893d54d7c6e3bc
azimjon0103 <azimjonfayzullayev3@gmail.com>
2022-10-20 16:15:06 +0500

    09

--- While09.py
+++ While09.py
@@ -6,4 +6,18 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i=0
+    ans1=0
+    ans2=0
+    while  i<len(s):
+        if s[i].isdigit():
+            if int(s[i])%2==0:
+                   ans1+=int(s[i])
+
+        if s[i].isdigit():                  
+            if int(s[i])%2==1:
+                   ans2+=int(s[i])
+        i+=1  
+    sum=ans1+ans2
+    return sum
+print(main('124'))     
